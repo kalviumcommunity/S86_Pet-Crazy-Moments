bc290bf276149c1f0e46fd97fb68cf2d8017f414
Bhavdeep-Sai <bhavdeepsai@gmail.com>
2025-02-27 10:13:15 +0530

    changes made according to ASAP M7

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+require('dotenv').config();
+//Mongodb Connection
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MongoDB_URI,{})
+.then(()=>console.log('Mongodb is connected'))
+.catch((err)=>console.log("Mongodb connection failed",err))
+
+
 app.get('/ping', (req, res) => {
     res.send('pong');
 });
